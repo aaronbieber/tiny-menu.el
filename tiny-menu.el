30508f8458b1f9d4396244df2cd786da588caf0b
Aaron Bieber <aaron@aaronbieber.com>
2016-05-26 06:29:49 -0400

    Add copyright, dependencies, truncate docs

--- tiny-menu.el
+++ tiny-menu.el
@@ -1,58 +1,35 @@
 ;;; tiny-menu.el --- Run a selected command from one menu.
 
+;; Copyright (c) 2016 Aaron Bieber
+;;
+;; Author: Aaron Bieber <aaron@aaronbieber.com>
+
+;; Package-Requires: ((emacs "24.4"))
+;; Keywords: menu tools
+;; Homepage: https://github.com/aaronbieber/tiny-menu.el
+
+;; Tiny Menu is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; Tiny Menu is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Tiny Menu.  If not, see http://www.gnu.org/licenses.
+
 ;;; Commentary:
 
-;; This is meant to be used simiarly to the much more feature-rich
-;; "Hydra" package where keys are "chained" to create groups of
-;; commands.  This package always presents an interactive menu in the
-;; minibuffer, and can also generate a dynamic menu of all available
-;; menus.
-;;
-;; To use the tiny menu, simply define the `tiny-menu-items'
-;; variable and then map keys to the items using the
-;; `tiny-menu-run-item' macro.
-;;
-;; `tiny-menu-items' is an alist of "items" where each "item" is a
-;; menu of available options.  A menu is a list whose car is the name
-;; of the menu and cdr is a list of menu items.  Each menu item is a
-;; list with three elements: the raw character code a user must press
-;; to select the item, the display name for the item, and the function
-;; to be called when the item is selected.
-;;
-;; For example, it might look like this:
-;;
-;; '(("buffer-menu" ("Buffer operations"
-;;                   ((?k "Kill" kill-this-buffer)
-;;                    (?b "Bury" bury-buffer))))
-;;   ("help-menu"   ("Help operations"
-;;                   ((?f "Describe function" describe-function)
-;;                    (?k "Describe key"      describe-key)))))
-;;
-;; This value defines two menus, one named "buffer-menu" and another
-;; named "help-menu".  You can now bind a key to display a menu of the
-;; menus by calling `tiny-menu' with no arguments, but the prompt
-;; letters will simply be alphabetical.  It is much more ergonomic to
-;; map specific keys to each of the menus, which is simplified by
-;; using the `tiny-menu-run-item' macro, like this:
-;; 
-;; (define-key some-map (kbd "<key>") (tiny-menu-run-item "buffer-menu"))
-;;
-;; The macro returns an anonymous interactive function suitable for
-;; key binding.
-;;
-;; It may also be useful (though not required) to use a custom prefix
-;; key if all of the menus are related.  This is covered in other
-;; Emacs documentation, but for the sake of convenience, this is how
-;; you could do that:
-;;
-;; (define-prefix-command tiny-menu-map)
-;; (define-key tiny-menu-map
-;;   (kbd "<menu-key-1>") (tiny-menu-run-item "menu-1"))
-;; (define-key tiny-menu-map
-;;   (kbd "<menu-key-2>") (tiny-menu-run-item "menu-2"))
-;; (define-key global-map (kbd "<prefix>") tiny-menu-map)
+;; Tiny Menu provides a simple mechanism for building one-line menus
+;; of commands suitable for binding to keys.  For a full description
+;; and examples of use, see the `README.md' file packaged along with
+;; this program.
 
 ;;; Code:
+
 (defface tiny-menu-heading-face
   '((t (:inherit 'font-lock-string-face)))
   "The menu heading shown in the selection menu for `tiny-menu'."
