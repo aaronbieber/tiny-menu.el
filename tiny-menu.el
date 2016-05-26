fb482254d454e733ba083739d6394993d4bbc2aa
Aaron Bieber <aaron@aaronbieber.com>
2016-05-25 21:24:50 -0400

    Renamed to tiny-menu

--- tiny-menu.el
+++ tiny-menu.el
@@ -1,4 +1,4 @@
-;;; tiny-menu-run.el --- Run a selected command from one menu.
+;;; tiny-menu.el --- Run a selected command from one menu.
 
 ;;; Commentary:
 
@@ -8,11 +8,11 @@
 ;; minibuffer, and can also generate a dynamic menu of all available
 ;; menus.
 ;;
-;; To use the tiny menu, simply define the `tiny-menu-run-items'
+;; To use the tiny menu, simply define the `tiny-menu-items'
 ;; variable and then map keys to the items using the
 ;; `tiny-menu-run-item' macro.
 ;;
-;; `tiny-menu-run-items' is an alist of "items" where each "item" is a
+;; `tiny-menu-items' is an alist of "items" where each "item" is a
 ;; menu of available options.  A menu is a list whose car is the name
 ;; of the menu and cdr is a list of menu items.  Each menu item is a
 ;; list with three elements: the raw character code a user must press
@@ -30,7 +30,7 @@
 ;;
 ;; This value defines two menus, one named "buffer-menu" and another
 ;; named "help-menu".  You can now bind a key to display a menu of the
-;; menus by calling `tiny-menu-run' with no arguments, but the prompt
+;; menus by calling `tiny-menu' with no arguments, but the prompt
 ;; letters will simply be alphabetical.  It is much more ergonomic to
 ;; map specific keys to each of the menus, which is simplified by
 ;; using the `tiny-menu-run-item' macro, like this:
@@ -53,17 +53,17 @@
 ;; (define-key global-map (kbd "<prefix>") tiny-menu-map)
 
 ;;; Code:
-(defface tiny-menu-run-heading-face
+(defface tiny-menu-heading-face
   '((t (:inherit 'font-lock-string-face)))
-  "The menu heading shown in the selection menu for `tiny-menu-run'."
-  :group 'tiny-menu-run)
+  "The menu heading shown in the selection menu for `tiny-menu'."
+  :group 'tiny-menu)
 
-(defvar tiny-menu-run-items
+(defvar tiny-menu-items
   '(())
   "An alist of menus.
 
 The keys in the alist are simple strings used to reference the menu in
-calls to `tiny-menu-run' and the values are lists with three elements:
+calls to `tiny-menu' and the values are lists with three elements:
 A raw character to use as the selection key, such as `?a'; a string to
 use in the menu display, and a function to call when that item is
 selected.
@@ -75,16 +75,16 @@ The data structure should look like:
   (\"menu-2\" (?z \"First item\" function-to-call-for-item-1)
             (?x \"Second item\" function-to-call-for-item-2)))")
 
-(defun tiny-menu-run (&optional menu)
+(defun tiny-menu (&optional menu)
   "Display the items in MENU and run the selected item.
 
 If MENU is not given, a dynamically generated menu of available menus
 is displayed."
   (interactive)
-  (if (< (length tiny-menu-run-items) 1)
-      (message "Configure tiny-menu-run-items first.")
-    (let* ((menu (if (assoc menu tiny-menu-run-items)
-                     (cadr (assoc menu tiny-menu-run-items))
+  (if (< (length tiny-menu-items) 1)
+      (message "Configure tiny-menu-items first.")
+    (let* ((menu (if (assoc menu tiny-menu-items)
+                     (cadr (assoc menu tiny-menu-items))
                    (air-menu-of-menus)))
            (title (car menu))
            (items (append (cadr menu)
@@ -94,7 +94,7 @@ is displayed."
                                         (concat
                                          (propertize (concat
                                                       "[" (char-to-string (nth 0 i)) "] ")
-                                                     'face 'tiny-menu-run-heading-face)
+                                                     'face 'tiny-menu-heading-face)
                                          (nth 1 i)))
                                       items ", ")))
                    (choices (mapcar (lambda (i) (nth 0 i)) items))
@@ -107,15 +107,15 @@ is displayed."
 (defun air-menu-of-menus ()
   "Build menu items for all configured menus.
 
-This allows `tiny-menu-run' to display an interactive menu of all
+This allows `tiny-menu' to display an interactive menu of all
 configured menus if the caller does not specify a menu name
 explicitly."
   (let ((menu-key-char 97))
     `("Menus" ,(mapcar (lambda (i)
                 (prog1
-                    `(,menu-key-char ,(car (car (cdr i))) (lambda () (tiny-menu-run ,(car i))))
+                    `(,menu-key-char ,(car (car (cdr i))) (lambda () (tiny-menu ,(car i))))
                   (setq menu-key-char (1+ menu-key-char))))
-              tiny-menu-run-items))))
+              tiny-menu-items))))
 
 (defmacro tiny-menu-run-item (item)
   "Return a function suitable for binding to call the ITEM run menu.
@@ -124,10 +124,10 @@ This saves you the trouble of putting inline lambda functions in all
 of the key binding declarations for your menus.  A key binding
 declaration now looks like:
 
-`(define-key some-map \"<key>\" (tiny-menu-run-item \"my-menu\"))'."
+`(define-key some-map \"<key>\" (tiny-menu-item \"my-menu\"))'."
   `(lambda ()
      (interactive)
-     (tiny-menu-run ,item)))
+     (tiny-menu ,item)))
 
-(provide 'tiny-menu-run)
-;;; tiny-menu-run ends here
+(provide 'tiny-menu)
+;;; tiny-menu ends here
